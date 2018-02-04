a7336f4250efabeb9498ffa952a6d3c9ad557e34
Burcak Ulug <burcakulug@gmail.com>
2018-02-04 14:52:15 -0500

    Generate UsersPage container

---
+++ app/containers/UsersPage/index.js
@@ -0,0 +1,59 @@
+/**
+ *
+ * UsersPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { FormattedMessage } from 'react-intl';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectUsersPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+import messages from './messages';
+
+export class UsersPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+        <Helmet>
+          <title>UsersPage</title>
+          <meta name="description" content="Description of UsersPage" />
+        </Helmet>
+        <FormattedMessage {...messages.header} />
+      </div>
+    );
+  }
+}
+
+UsersPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  userspage: makeSelectUsersPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'usersPage', reducer });
+const withSaga = injectSaga({ key: 'usersPage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(UsersPage);
