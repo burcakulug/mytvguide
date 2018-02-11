924e92e6583852db6a6292ac3ba52c857bcefa2c
Burcak Ulug <burcakulug@gmail.com>
2018-02-11 17:46:41 -0500

    Create a styled Grid component for Shows, Seasons and Episodes in  Home

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -11,13 +11,30 @@
 
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import styled from 'styled-components';
 import messages from './messages';
+
+import Shows from '../Shows';
+import Seasons from '../Seasons';
+import Episodes from '../Episodes';
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-areas:
+    "shows seasons episodes";
+  grid-template-columns: 1fr 1fr 1fr
+`;
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <h1>
         <FormattedMessage {...messages.header} />
+        <Grid>
+          <Shows />
+          <Seasons />
+          <Episodes />
+        </Grid>
       </h1>
     );
   }
