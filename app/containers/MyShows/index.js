b0ec95e3243f5d129ec63fa44d572a3403e6ab44
Burcak Ulug <burcakulug@gmail.com>
2018-02-25 10:46:44 -0500

    Display my shows in MyShows container

--- app/containers/MyShows/index.js
+++ app/containers/MyShows/index.js
@@ -11,6 +11,8 @@ import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
+import { Grid, Cell } from 'styled-css-grid';
+import { List, ListItem } from 'material-ui';
 
 import injectSaga from 'utils/injectSaga';
 import injectReducer from 'utils/injectReducer';
@@ -21,13 +23,36 @@ import messages from './messages';
 
 export class MyShows extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
+    const { showData } = this.props.myShows;
     return (
       <div>
         <Helmet>
           <title>MyShows</title>
           <meta name="description" content="Description of MyShows" />
         </Helmet>
-        <FormattedMessage {...messages.header} />
+        <h2><FormattedMessage {...messages.header} /></h2>
+        <Grid columns={3}>
+          <Cell>
+            <h3>Shows</h3>
+            <List>
+              {showData && showData.map((data) => {
+                console.log('data', data);
+                return (
+                  <ListItem key={data.show.id}>
+                    {data.show.id} - {data.show.name}
+                  </ListItem>);
+              }
+              )
+              }
+            </List>
+          </Cell>
+          <Cell>
+            <h3>Seasons</h3>
+          </Cell>
+          <Cell>
+            <h3>Episodes</h3>
+          </Cell>
+        </Grid>
       </div>
     );
   }
@@ -38,7 +63,7 @@ MyShows.propTypes = {
 };
 
 const mapStateToProps = createStructuredSelector({
-  myshows: makeSelectMyShows(),
+  myShows: makeSelectMyShows(),
 });
 
 function mapDispatchToProps(dispatch) {
